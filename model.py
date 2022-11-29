74a37f78ac44e4be172532b8e682537d75ac9987
bsimjoo-official <b.simjoo.mail@gmail.com>
2022-11-29 16:49:41 +0330

    add to_dict method to all models

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from json import load
-from peewee import SqliteDatabase, Model, TextField, CharField, BooleanField, ForeignKeyField, DateTimeField, IntegerField, DateField
+from peewee import SqliteDatabase, Model, TextField, FixedCharField, BooleanField, ForeignKeyField, DateTimeField, IntegerField, DateField, TimeField
 from datetime import datetime
+from playhouse.shortcuts import model_to_dict
 
 config:dict = load(open('config.json','r'))
 
@@ -10,22 +11,31 @@ class BaseModel(Model):
     class Meta:
         database = db
         
-class Meeting(BaseModel):   # Chosen meeting because to prevent collide with flask session
-    date = DateField(default = datetime.now)
+    def to_dict(self,**kwargs)->dict:
+        res = model_to_dict(self)
+        res.update(**kwargs)
+        return res
+        
+class Meeting(BaseModel):   # Chosen meeting name because to prevent collide with flask session
+    date = DateField(default = lambda:datetime.now().date())
     # attendances
+    # scores
     
 
 class Student(BaseModel):
     name = TextField(unique=True)
-    number = CharField(max_length=11,unique=True)
+    number = FixedCharField(max_length=11,unique=True)
     # scores
     # attendances
     # devices
     
     @property
     def total_score(self):
-        return sum(map(lambda x:x.score,self.scores))  # type: ignore
-            
+        return sum(map(lambda x:x.score,self.scores))           # type: ignore
+    
+    def to_dict(self, **kwargs) -> dict:
+        return super().to_dict(total_score = self.total_score, **kwargs)
+    
 
 class Device(BaseModel):  # type: ignore
     mac = TextField(index=True,unique=True)
@@ -37,9 +47,10 @@ class Attendance(BaseModel):  # type: ignore
     student = ForeignKeyField(Student,backref = 'attendances')
     device = ForeignKeyField(Device,backref = 'login_history')
     meeting = ForeignKeyField(Meeting, backref= 'attendances')
+    time = TimeField(default = lambda: datetime.now().time())
     
 class Score(BaseModel):
     student = ForeignKeyField(Student,backref="scores")
     score = IntegerField()
-    date_time = DateTimeField(default = datetime.now)
+    meeting = ForeignKeyField(Meeting, null=True, backref='scores')
     reason = TextField(null=True)
